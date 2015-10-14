ab082c528ace9b4d022ba8dd9ed9ba3f38d18e5d
Marlon Raphael <raphael27atm@gmail.com>
2015-10-14 15:58:34 -0300

    setup bootstrap

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 
 gem 'rails', '4.2.4'
+gem 'bootstrap-sass', '~>3.3.5'
+gem 'autoprefixer-rails', '~>5.2.0'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+Rspec.feature "Creating Articles" do
+  scenario "A user a new article" do
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: "Create first Article"
+    fill_in "Body", with: "lorem Ipsum"
+    click_botton "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
+
+  end
+end
